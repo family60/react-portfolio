136af74dbb651b5abf4bee5499f27097f735e2cb
Kush Patel <kushrpatel06@yahoo.com>
2023-01-31 17:50:41 -0500

    Working on homepage and nav bar got logo and link icons/images working properly

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,22 +2,29 @@ import React from "react";
 
 export default function Nav({currentPage, handlePageChange}){
     return(
-        <div>
-            <p className="initials">KP.</p>
-            <ul className="nav nav-tabs">
-                <li className="nav-item">
-                    <a href="#home" onClick={() => handlePageChange("Home")} className={currentPage ==="Home" ? "nav-link active" : "nav-link"}>Home</a>
-                </li>
-                <li className="nav-item">
-                    <a href="#about" onClick={() => handlePageChange("About")} className={currentPage ==="About" ? "nav-link active" : "nav-link"}>About</a>
-                </li>
-                <li className="nav-item">
-                    <a href="#projects" onClick={() => handlePageChange("Projects")} className={currentPage ==="Projects" ? "nav-link active" : "nav-link"}>Projects</a>
-                </li>
-                <li className="nav-item">
-                    <a href="#contact" onClick={() => handlePageChange("Contact")} className={currentPage ==="Contact" ? "nav-link active" : "nav-link"}>Contact</a>
-                </li>
-            </ul>
+        <div className="container">
+            <div className="row">
+                <p className="col-md-2 initials">KP;</p>
+                <ul className="nav nav-pills col-md-7">
+                    <li className="nav-item">
+                        <a href="#home" onClick={() => handlePageChange("Home")} className={currentPage ==="Home" ? "nav-link active" : "nav-link"}>Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#about" onClick={() => handlePageChange("About")} className={currentPage ==="About" ? "nav-link active" : "nav-link"}>About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#projects" onClick={() => handlePageChange("Projects")} className={currentPage ==="Projects" ? "nav-link active" : "nav-link"}>Projects</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#contact" onClick={() => handlePageChange("Contact")} className={currentPage ==="Contact" ? "nav-link active" : "nav-link"}>Contact</a>
+                    </li>
+                </ul>
+                <div className="col-md-3 icons">
+                    <a href="https://www.linkedin.com/in/kush-patel-b73230202/" target="_blank"><img className="icon" src={require("../../src/assets/images/linkedin.png")}/></a>
+                    <a href="https://github.com/family60" target="_blank"><img className="icon" src={require("../../src/assets/images/github.png")}/></a>
+                    <a href={require("../../src/assets/Resume.pdf")} target="_blank"><img className="icon" src={require("../../src/assets/images/resume_icon.png")}/></a>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,8 +2,15 @@ import React from "react";
 
 export default function Home() {
     return(
-        <div>
-            HOME!!
+        <div className="home-content container">
+            <div className="row">
+                <div className="col-md-7">
+                    <h1>Hi! I'm Kush Patel</h1>
+                </div>
+                <div className="col-md-5">
+                    <img className="portfolio-pic" src={require("../../assets/images/snorlax.png")} alt="snorlax img"/>
+                </div>
+            </div>
         </div>
     );
 };
