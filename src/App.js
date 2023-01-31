3e1ada0844b1009d185a65316701fd3396ac7406
Kush Patel <kushrpatel06@yahoo.com>
2023-01-31 15:19:11 -0500

    Added navigation bar functionality deleted app.css and index.css to import bootstrap created pages folder to hold components that are supposed to be rendered as pages added pages components with basic code to get mvp running added basic navbar functionality using portfolio container and nav components App component now only returns portfolio container component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-import './App.css';
+import PortfolioContainer from './components/PortfolioContainer';
 
-function App() {
-  return (
-    <div>
-      KP.
-    </div>
-  );
-}
+const App = () => <PortfolioContainer/>;
 
 export default App;
