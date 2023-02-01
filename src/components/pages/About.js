3a6d34a179fdaf99eefae3a8ff49df644504056b
Kush Patel <kushrpatel06@yahoo.com>
2023-01-31 21:30:13 -0500

    working on about page content

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,20 +2,32 @@ import React from "react";
 
 export default function About() {
     return(
-        <div>
-            <div>
-                <h2>Me</h2>
+        <div className="about-content container">
+            <div className="about-section">
+                <h2>About Me</h2>
                 <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vulputate volutpat tristique. Maecenas dictum augue a arcu vestibulum, suscipit luctus elit convallis. Vestibulum id ultricies lectus. Vivamus quis elit quis nulla porta pulvinar sed vel turpis. Fusce fringilla sed quam a gravida. Suspendisse luctus efficitur nunc, vitae mattis nibh. Nullam feugiat eleifend eros. In hac habitasse platea dictumst. Praesent quam magna, facilisis in dui sed, gravida aliquam sapien. Maecenas a tristique ipsum. Donec eleifend ornare orci, sed sagittis justo porta sit amet. Nullam mi nibh, venenatis in orci rhoncus, tincidunt semper massa. Duis eu nisl urna. Pellentesque lacinia orci et justo eleifend placerat. Aliquam euismod ultrices justo quis sodales. 
+                    I've always had a natural curiosity towards technology and so it was only a matter of time that
+                    I would take up the chance to pursue a career in it. For me, coding gives me the ability to make
+                    a real difference in the real world, provide value and solve problems that really matter. I want to keep
+                    learning more about web development and new technologies to grow my skill set. I hope to meet and work with
+                    like minded individuals. When I'm not coding, you can find me going on drives, reading, 
+                    playing video games, rewatching my favourite shows and movies, etc.
                 </p>
             </div>
-            <div>
+            <div className="about-section">
                 <h2>Education</h2>
                 <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vulputate volutpat tristique. Maecenas dictum augue a arcu vestibulum, suscipit luctus elit convallis. Vestibulum id ultricies lectus. Vivamus quis elit quis nulla porta pulvinar sed vel turpis. Fusce fringilla sed quam a gravida. Suspendisse luctus efficitur nunc, vitae mattis nibh. Nullam feugiat eleifend eros. In hac habitasse platea dictumst. Praesent quam magna, facilisis in dui sed, gravida aliquam sapien. Maecenas a tristique ipsum. Donec eleifend ornare orci, sed sagittis justo porta sit amet. Nullam mi nibh, venenatis in orci rhoncus, tincidunt semper massa. Duis eu nisl urna. Pellentesque lacinia orci et justo eleifend placerat. Aliquam euismod ultrices justo quis sodales. 
+                    I graduated from Humber college with a diploma in "Computer Programming". My graduation happened
+                    during the pandemic and so pursuing further studies became difficult as most birdge programs were no longer supported.
+                    I decided to take a free online certification in "Javascript Algorithms and Data Structures" to pass time and try to learn coding on my own.
+                    I quickly realized how intimidating learning coding on your own can be and how real imposter syndrome is when looking for jobs.
+                    After not finding much success - despite the many opportunities I was taking - I decided to return to my studies
+                    at the University of Toronto, in their school of continuing studies. There, I received a certificate for successfully completing
+                    their "Full-Stack Flex" Bootcamp. Here, I became a fully certified Full-Stack developer and even added many
+                    different technologies and tools to my arsenal.
                 </p>
             </div>
-            <div>
+            <div className="about-section">
                 <h2>Technologies and Skills</h2>
                 <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vulputate volutpat tristique. Maecenas dictum augue a arcu vestibulum, suscipit luctus elit convallis. Vestibulum id ultricies lectus. Vivamus quis elit quis nulla porta pulvinar sed vel turpis. Fusce fringilla sed quam a gravida. Suspendisse luctus efficitur nunc, vitae mattis nibh. Nullam feugiat eleifend eros. In hac habitasse platea dictumst. Praesent quam magna, facilisis in dui sed, gravida aliquam sapien. Maecenas a tristique ipsum. Donec eleifend ornare orci, sed sagittis justo porta sit amet. Nullam mi nibh, venenatis in orci rhoncus, tincidunt semper massa. Duis eu nisl urna. Pellentesque lacinia orci et justo eleifend placerat. Aliquam euismod ultrices justo quis sodales. 
